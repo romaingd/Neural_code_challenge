607fdb7901651d12f6cb776ef7a531221d645305
romaingd <34568327+romaingd@users.noreply.github.com>
2019-02-22 10:19:39 +0100

    CDF features and KNN

--- neighbors_isi.py
+++ neighbors_isi.py
@@ -15,13 +15,14 @@ from classification import classify
 # Parameters
 data_folder = './data/'
 isi_folder = './features/isi/'
+cdf_folder = './features/CDF/'
 submission_folder = './submissions/neighbors_isi/'
 
 recompute_features = False
-use_precomputed = True
+use_precomputed = False
 
-perform_evaluation = True
-perform_cross_validation = False
+perform_evaluation = False
+perform_cross_validation = True
 
 compute_submission = False
 
@@ -52,7 +53,7 @@ if recompute_features:
 
         feat_tr = pd.DataFrame(data=feat_tr_val, index=x_tr.index)
         feat_tr['neuron_id'] = x_tr['neuron_id']
-        feat_tr.to_csv('/features/KS/feat_tr.csv', index=True, header=True)
+        feat_tr.to_csv('./features/KS/feat_tr.csv', index=True, header=True)
         del feat_tr
 
 
@@ -60,9 +61,41 @@ if recompute_features:
 
         feat_te = pd.DataFrame(data=feat_te_val, index=x_te.index)
         feat_te['neuron_id'] = x_te['neuron_id']
-        feat_te.to_csv('/features/KS/feat_te.csv', index=True, header=True)
+        feat_te.to_csv('./features/KS/feat_te.csv', index=True, header=True)
         del feat_te
+    
+    elif features_to_compute == 'CDF':
+        x_tr, x_te, y_tr = load_data(
+            features_folder=isi_folder,
+            data_folder=data_folder
+        )
 
+        x_tr_val = x_tr.drop(columns=['neuron_id']).values
+        x_te_val = x_te.drop(columns=['neuron_id']).values
+
+        _, full_bins = np.histogram(np.log(x_tr_val.ravel()), bins=100)
+
+        f = lambda a: np.histogram(a, bins=full_bins, density=True)[0]
+        print('Computing training features...')
+        feat_tr_val = np.cumsum(
+            np.apply_along_axis(func1d=f, axis=1, arr=x_tr_val),
+            axis=1
+        )
+        print('Computing test features...')
+        feat_te_val = np.cumsum(
+            np.apply_along_axis(func1d=f, axis=1, arr=x_te_val),
+            axis=1
+        )
+
+        feat_tr = pd.DataFrame(data=feat_tr_val, index=x_tr.index)
+        feat_tr['neuron_id'] = x_tr['neuron_id']
+        feat_tr.to_csv('./features/' + features_to_compute + '/feat_tr.csv', index=True, header=True)
+        del feat_tr
+
+        feat_te = pd.DataFrame(data=feat_te_val, index=x_te.index)
+        feat_te['neuron_id'] = x_te['neuron_id']
+        feat_te.to_csv('./features/' + features_to_compute + '/feat_te.csv', index=True, header=True)
+        del feat_te
 
 ###############################################################################
 #                                                                             #
@@ -83,6 +116,12 @@ best_params = {
     'KS': {
         'n_neighbors': 10,
         'n_jobs': -1,
+    },
+    'CDF_L2': {
+        'n_neighbors': 50,
+        'weights': 'uniform',
+        'leaf_size': 15,
+        'n_jobs': -1
     }
 }
 cv_params = {
@@ -94,15 +133,22 @@ cv_params = {
     },
     'KS': {
         'n_neighbors': [5, 10, 15]
+    },
+    'CDF_L2': {
+        'n_neighbors': [50],
+        'weights': ['uniform'],
+        'leaf_size': [15],
+        'p': [2]
     }
 }
 est_list = {
     'Euclidean': KNeighborsClassifier(**best_params['Euclidean'], p=2),
     'MinkowskiL1': KNeighborsClassifier(**best_params['MinkowskiL1'], p=1),
     'KS': KNeighborsClassifier(**best_params['KS'], metric=kolmogorov_smirnov),
+    'CDF_L2': KNeighborsClassifier(**best_params['CDF_L2'], p=2),
 }
 
-est_name = 'KS'
+est_name = 'CDF_L2'
 
 
 # Make sure we're not trying to load unavailable pre-computed features
@@ -117,6 +163,11 @@ if use_precomputed:
         data_folder=data_folder
     )
     est_list[est_name].set_params(metric='precomputed')
+elif est_name[:3] == 'CDF':
+    x_tr, x_te, y_tr = load_data(
+        features_folder=cdf_folder,
+        data_folder=data_folder
+    )
 else:
     x_tr, x_te, y_tr = load_data(
         features_folder=isi_folder,
@@ -156,5 +207,5 @@ clf = classify(
     cv_params=cv_params[est_name],
     compute_submission=compute_submission,
     submission_path=(submission_folder + 'y_te_pred.csv'),
-    random_state=42
+    random_state=None
 )
